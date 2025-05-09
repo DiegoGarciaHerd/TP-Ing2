1226bd896acf39c287acf54e6f7e6c0031b17b07
Diego Garcia Herd <114368736+DiegoGarciaHerd@users.noreply.github.com>
2025-05-09 14:28:01 -0300

    Instalo dependencias

---
+++ autorental/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for autorental project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autorental.settings')
+
+application = get_wsgi_application()
+http://127.0.0.1:8000
