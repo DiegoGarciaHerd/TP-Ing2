cddad0bf78f03e1e7f3c9e832f19d2846821915c
Diego Garcia Herd <114368736+DiegoGarciaHerd@users.noreply.github.com>
2025-05-12 11:50:24 -0300

    Resuelvo problemas con wsgi.py

--- autorental/wsgi.py
+++ autorental/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autorental.settings')
 
 application = get_wsgi_application()
+application = get_wsgi_application()
+# http://127.0.0.1:8000
