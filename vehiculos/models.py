a20accca473fb834436e1a2df36813f131c00135
Juan Ignacio Coelho Soria <lapampa2003@gmail.com>
2025-05-27 14:35:26 -0300

    Politica de reembolso añadida

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from sucursales.models import Sucursal
 
 class Vehiculo(models.Model):
+    REEMBOLSO_CHOICES = [
+        ('0', '0%'),
+        ('20', '20%'),
+        ('100', '100%'),
+    ]
+
     marca = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     año = models.PositiveIntegerField()
@@ -10,6 +16,11 @@ class Vehiculo(models.Model):
     sucursal_actual = models.ForeignKey(Sucursal, on_delete=models.CASCADE, related_name='vehiculos_en_sucursal')
     disponible = models.BooleanField(default=True)
     foto_base64 = models.TextField(blank=True, null=True)
+    politica_de_reembolso = models.CharField(
+        max_length=3,
+        choices=REEMBOLSO_CHOICES,
+        default='0'
+    )
 
     def __str__(self):
         return f"{self.marca} {self.modelo} ({self.patente})"
