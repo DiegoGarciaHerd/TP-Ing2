8196ebcb9cf15578fdf17e2813e808935619aefb
Diego Garcia <diegogarciaherd@hotmail.com.ar>
2025-05-29 18:12:04 -0300

    prevenir que el admin pueda entrar desde el login de usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+from .views import UsuarioLoginView
 
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
-    path('login/', views.login_view, name='login'),
+    path('login/', UsuarioLoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('editar-perfil/', views.editar_perfil, name='editar_perfil'),
     path('recuperar-password/', views.recuperar_password, name='recuperar_password'),
 ]
-
-
 
