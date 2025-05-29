abe6bd0cbde9b15d62882fe47299857bb9bda15f
Juan Ignacio Coelho Soria <lapampa2003@gmail.com>
2025-05-29 18:29:48 -0300

    Pusheado todo

--- administrador/urls.py
+++ administrador/urls.py
@@ -11,16 +11,6 @@ from .views import (
 )
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('login/', views.login_admin_step1, name='admin_login_step1'),
-    path('verificar/', views.login_admin_step2, name='admin_login_step2'),
-    path('menu-admin/', views.admin_menu, name="admin_menu"),
-    path('carga-autos/', views.cargar_autos, name="cargar_autos"),
-    path('borrar-autos/', views.borrar_autos, name="borrar_autos"),
-    path('carga-empleados/', views.cargar_empleados, name="cargar_empleados"),
-    path('modificar-autos/', views.modificar_autos, name="modificar_autos"),
-    path('ver-autos/', views.ver_autos, name="ver_autos")
-=======
     path('login/', login_admin_step1, name='admin_login_step1'),
     path('verificar/', login_admin_step2, name='admin_login_step2'),
     path('menu-admin/', admin_menu, name="admin_menu"),
@@ -29,5 +19,4 @@ urlpatterns = [
     path('carga-empleados/', cargar_empleados, name="cargar_empleados"),
     path('modificar-autos/', modificar_autos, name="modificar_autos"),
     path('logout/', admin_logout, name='admin_logout'),
->>>>>>> 27f29f389c8f9595a4b215430fc39597d1ba2d4b
 ]
