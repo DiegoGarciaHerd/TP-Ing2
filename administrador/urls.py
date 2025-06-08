d9df14bf24272ed42be22d3feea940032a1e94b2
Conrado Federico Escobares <conradofecobares@gmail.com>
2025-06-08 13:45:24 -0300

    Cambios locales en urls

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from .views.auth import (
-    login_admin_step1,
+    login_admin_step1 as admin_login,
     login_admin_step2,
-    admin_logout,
-    admin_menu
+    admin_menu,
+    admin_logout
 )
 from .views.vehiculos import (
     cargar_autos,
-    borrar_autos,
     modificar_autos,
+    borrar_autos,
     ver_autos,
-    obtener_datos_vehiculo
+    obtener_datos_vehiculo,
+    toggle_disponibilidad
 )
-from .views.empleados import cargar_empleados
+from .views.empleados import cargar_empleados, modificar_empleados, ListarEmpleadosView
 
 from .views.sucursal import (
     cargar_sucursal,
@@ -21,17 +22,30 @@ from .views.sucursal import (
 )
 
 urlpatterns = [
-    path('login/', login_admin_step1, name='admin_login_step1'),
-    path('verificar/', login_admin_step2, name='admin_login_step2'),
-    path('menu-admin/', admin_menu, name="admin_menu"),
-    path('carga-autos/', cargar_autos, name="cargar_autos"),
-    path('borrar-autos/', borrar_autos, name="borrar_autos"),
-    path('carga-empleados/', cargar_empleados, name="cargar_empleados"),
-    path('modificar-autos/', modificar_autos, name="modificar_autos"),
-    path('obtener-datos-vehiculo/', obtener_datos_vehiculo, name="obtener_datos_vehiculo"),
-    path('ver-autos/', ver_autos, name="ver_autos"),
+
+
+    path('', admin_menu, name='admin_menu'),  # Menú en la raíz de admin-auth/Add commentMore actions
+    path('login/', admin_login, name='admin_login_step1'),  # Renombrado para claridad
+    path('verificar/', login_admin_step2, name='admin_login_step2'),  # Agregado el segundo paso
     path('logout/', admin_logout, name='admin_logout'),
+
+
+
+    # URLs de Vehiculos
+    path('carga-autos/', cargar_autos, name="cargar_autos"),
+    path('modificar-autos/', modificar_autos, name="modificar_autos"),
+    path('borrar-autos/', borrar_autos, name="borrar_autos"),
+    path('ver-autos/', ver_autos, name="ver_autos"),
+    path('obtener-datos-vehiculo/', obtener_datos_vehiculo, name="obtener_datos_vehiculo"),
+     path('ver-autos/<int:vehiculo_id>/toggle-disponibilidad/', toggle_disponibilidad, name="toggle_disponibilidad"),
+    
+    # URLs de Empleados
+    path('cargar-empleados/', cargar_empleados, name="cargar_empleados"),
+    path('modificar-empleados/', modificar_empleados, name="modificar_empleados"),
+    path('listar-empleados/', ListarEmpleadosView.as_view(), name="listar_empleados"),
+    
+    # URLs de Sucursales
     path('cargar-sucursal/', cargar_sucursal, name="cargar_sucursal"),
     path('modificar-sucursal/', modificar_sucursal, name="modificar_sucursal"),
-    path('borrar-sucursal/', borrar_sucursal, name="borrar_sucursal")
+    path('borrar-sucursal/', borrar_sucursal, name="borrar_sucursal"),
 ]
