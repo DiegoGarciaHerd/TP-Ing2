f0f49b06510c0df405324064026dee88eb3d16bb
Diego Garcia <diegogarciaherd@hotmail.com.ar>
2025-07-04 20:36:03 -0300

    Cambio detalle

--- empleados/forms.py
+++ empleados/forms.py
@@ -30,11 +30,8 @@ class EmpleadoEditarPerfilForm(forms.ModelForm):
     
     class Meta:
         model = Empleado
-        fields = ['nombre', 'apellido', 'email', 'direccion', 'telefono']
+        fields = ['direccion', 'telefono']
         widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'direccion': forms.TextInput(attrs={'class': 'form-control'}),
             'telefono': forms.TextInput(attrs={'class': 'form-control'}),
         }
